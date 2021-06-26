aa4ea396fc44464baf0c77120694e28d5af0d193
Fabián <ramirezfabian003@gmail.com>
2021-06-25 18:08:15 -0600

    Funciona descargar archivos

--- XTEC-tutor/src/app/Estudiante/ver-entrada/ver-entrada.component.ts
+++ XTEC-tutor/src/app/Estudiante/ver-entrada/ver-entrada.component.ts
@@ -11,18 +11,46 @@ import {FileService} from "../../_service/file.service";
 })
 export class VerEntradaComponent implements OnInit {
   public id: any;
+  public message: any;
   public entrada: any;
-  public fechaCorregida: any;
-  constructor(private route: ActivatedRoute, private fileService: FileService, private datepipe: DatePipe,) { }
+  constructor(private route: ActivatedRoute, public fileService: FileService, private datepipe: DatePipe,) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params.id;
     this.obtenerEntrada()
   }
   obtenerEntrada(){
-    this.fileService.obtenerEntrada(this.id).subscribe((resp:any)=>{this.entrada = resp})
+    this.fileService.obtenerEntrada(this.id).subscribe((resp:any)=>{
+      this.entrada = resp;
+    })
   }
   transformDate(){
      return this.datepipe.transform(this.entrada.fechaCreacion, 'dd/MM/yyyy');
+  }
+
+  download() {
+    this.fileService.download(this.id).subscribe((event) => {
+      this.message = 'Download success.';
+      console.log(event);
+      var contentDispositionHeader = event.headers.get('Content-Disposition');
+      // @ts-ignore
+      var result = contentDispositionHeader.split(';')[1].trim().split('=')[1];
+      const blob = event.body;
+      this.downloadFile(<Blob>blob, result.replace(/"/g, ''));
+
+    });
+  }
+
+  private downloadFile(data: Blob, filename:string) {
+    console.log(data);
+    const downloadedFile = new Blob([data], { type: data.type});
+    const a = document.createElement('a');
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+    a.download = filename;
+    a.href = URL.createObjectURL(downloadedFile);
+    a.target = '_blank';
+    a.click();
+    document.body.removeChild(a);
   }
 }
