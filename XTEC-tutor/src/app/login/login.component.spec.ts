8af4136a0ccb1d48f26c8bc46c2780820728dbb5
Fabián <ramirezfabian003@gmail.com>
2021-06-26 20:27:35 -0600

    Pruebas de integración del componente Login

--- XTEC-tutor/src/app/login/login.component.spec.ts
+++ XTEC-tutor/src/app/login/login.component.spec.ts
@@ -1,25 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login.component';
+import {FileService} from "../_service/file.service";
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule ],
+      providers: [FileService]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('Servicio creado', inject([FileService], (service: FileService) => {
+    expect(service).toBeTruthy();
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it ('Verificar a usuario registrado', async(() => {
+    const service: FileService = TestBed.get(FileService);
+    service.login('2018099536', 'f').then(tipo => {
+      const type:any = tipo;
+      expect(type).toBe(2);
+    });
+  }));
+
+
 });
